c73686eb8022d3d52932273898729aba1bc998cf
Nikhil Agarwal <email.nikhil.agarwal@gmail.com>
2025-03-18 04:44:46 +0800

    finished agent 1

--- agents/simple_nn_agent/game_runner.py
+++ agents/simple_nn_agent/game_runner.py
@@ -7,35 +7,46 @@ MODEL = ModelAttention.load_model("models/1300_model0nn.pth")
 
 move_count = 0
 
-state_dict = {'white': 'model', 'black': 'player'}
+state_dict = {'white': 'player', 'black': 'model'}
 
 translator = {0: 'white', 1: 'black'}
 
+def run_model_move():
+    input("Enter for model move\n")
+    encoded_board = encode_board(BOARD)
+    input_tensor = get_input_tensor(encoded_board, is_white_turn)
+    output = MODEL.forward(input_tensor)
+    probs = torch.exp(output[0])
+    probs_list = probs.tolist()[0]
+    src_cells, dst_cells = parse_cells(probs_list, is_white_turn, BOARD)
+    sorted_legal_moves = get_sorted_legal_moves(src_cells, dst_cells, BOARD)
+    chosen_move = choose_move(sorted_legal_moves)
+    make_move(chosen_move, BOARD)
+    print(f"MODEL-{translator[move_count % 2]} moved from {chosen_move[0:2]} to {chosen_move[2:]}")
+
+def run_player_move():
+    valid_input = False
+    while not valid_input:
+        input_move = input("Please enter a move\n")
+        input_move = input_move.lower()
+        is_valid_move = check_valid_move(input_move, BOARD)
+        if is_valid_move:
+            make_move(input_move, BOARD)
+            valid_input = True
+            print(f"Player moved from {input_move[0:2]} to {input_move[2:]}")
+
 while not BOARD.is_game_over():
     is_white_turn = move_count % 2 == 0
-    if is_white_turn and state_dict['white'] == 'model':
-        # if True:
-        input("Ready for Model Move??")
-        encoded_board = encode_board(BOARD)
-        input_tensor = get_input_tensor(encoded_board, is_white_turn)
-        output = MODEL.forward(input_tensor)
-        probs = torch.exp(output[0])
-        probs_list = probs.tolist()[0]
-        src_cells, dst_cells = parse_cells(probs_list, is_white_turn, BOARD)
-        sorted_legal_moves = get_sorted_legal_moves(src_cells, dst_cells, BOARD)
-        chosen_move = choose_move(sorted_legal_moves)
-        make_move(chosen_move, BOARD)
-        print(f"MODEL-{translator[move_count%2]} moved from {chosen_move[0:2]} to {chosen_move[2:]}")
+    if is_white_turn:
+        if state_dict['white'] == 'model':
+            run_model_move()
+        else:
+            run_player_move()
     else:
-        valid_input = False
-        while not valid_input:
-            input_move = input("Please enter a move")
-            input_move = input_move.lower()
-            is_valid_move = check_valid_move(input_move, BOARD)
-            if is_valid_move:
-                make_move(input_move, BOARD)
-                valid_input = True
-                print(f"Player moved from {input_move[0:2]} to {input_move[2:]}")
+        if state_dict['black'] == 'player':
+            run_player_move()
+        else:
+            run_model_move()
     move_count += 1
 
 outcome = BOARD.outcome()  # Retrieves the game outcome
